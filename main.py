74af64d9636ee73705a17211cbbbdc40455ceee3
Javier Muñoz Mendi <55241761+jmunozmendi@users.noreply.github.com>
2026-02-20 20:09:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -260,8 +260,7 @@ if __name__ == "__main__":
                     break
                 time.sleep(0.25)
             else:
-                if int(time.time() - start) % 1 == 0:
-                    print("⏳ Slot not available yet")
+                print("⏳ Slot not available yet")
                 time.sleep(0.25)
 
         if booked:
@@ -272,6 +271,7 @@ if __name__ == "__main__":
             print("❌ Booking failed")
 
         print("🔁 Waiting for next cycle\n")
+
 
 
 
